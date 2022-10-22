1291d61c931db013b3b9c5da7c3ab3a4e5e332d6
smartcontracts0 <116455562+smartcontracts0@users.noreply.github.com>
2022-10-22 21:24:54 +0400

    Create RawGenomicNFTs.sol

---
+++ RawGenomicNFTs.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol"; //Imported in case burning NFTs is used
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GenomicDataNFTs is ERC721URIStorage, Ownable{
+    uint public tokenCount;
+    constructor () ERC721("Raw Genomic Data NFTs", "RGD"){} // The constructor uses the imported ERC721 contract which requires two inputs in its constructor, the name and symbol of the NFT
+    
+    //Optional: Make only authorized users mint Raw Genomics Data NFT
+    function mint(string memory _tokenURI) external returns(uint){
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount); 
+        _setTokenURI(tokenCount, _tokenURI);
+        return(tokenCount);
+    } 
+
+
+
+}
