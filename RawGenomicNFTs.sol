a332680e8018c20832b92455cee47cdba9e454bf
smartcontracts0 <116455562+smartcontracts0@users.noreply.github.com>
2022-10-25 10:27:14 +0400

    Update RawGenomicNFTs.sol

--- RawGenomicNFTs.sol
+++ RawGenomicNFTs.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol"; //Imported in case burning NFTs is used
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GenomicDataNFTs is ERC721URIStorage, Ownable{
+contract RawGenomicDataNFTs is ERC721URIStorage, Ownable{
     uint public tokenCount;
     constructor () ERC721("Raw Genomic Data NFTs", "RGD"){} // The constructor uses the imported ERC721 contract which requires two inputs in its constructor, the name and symbol of the NFT
     
